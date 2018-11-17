637bd41aaa69c15bfce9ba4e787b7ba408e643e4
Elsa <Elsa@jean.com>
2018-11-17 21:18:32 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+
+		<title>My online Shop</title>
+
+	<link rel="stylesheet" href="styles/style.css" media="all"/>
+
+	</head>
+
+<body>
+
+	<div class="main_wrapper">
+
+		<div>This is a header!</div>
+
+		<div>This is a menubar</div>
+
+		<div>This is sidebar</div>
+
+		<div>This is content area</div>
+
+		<div>This is the footer</div>
+
+
+	</div>
+
+
+</body	
+
+</html>
